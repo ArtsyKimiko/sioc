758b89cd1bc83689a23584a8ec70069f0b63e226
ArtsyKimiko <114069062+ArtsyKimiko@users.noreply.github.com>
2023-11-24 20:45:27 +0000

    Ostatnie poprawki

--- Sprawozdanie 2/zad1.py
+++ Sprawozdanie 2/zad1.py
@@ -4,6 +4,9 @@ import scipy.interpolate as scp
 from sklearn import metrics
 import os
 
+from scipy.interpolate import interp1d
+from sklearn import metrics
+
 N = 100
 
 def sin(x):
@@ -21,7 +24,7 @@ def interpolate(x, y, method, filename):
     interpolated_function = scp.interp1d(x, y, kind=method)
     x_int = np.linspace(x.min(), x.max(), N)
     y_int = interpolated_function(x_int)
-    y_true = sin(x_int)
+    y_true = np.sin(x_int)
 
     print(f"MSE: {metrics.mean_squared_error(y_pred=y_int, y_true=y_true):.8f}")
 
@@ -32,8 +35,8 @@ def interpolate(x, y, method, filename):
 
     script_dir = os.path.dirname(__file__)
     save_path = os.path.join(script_dir, filename)
-
     plt.savefig(save_path)
+    
     plt.clf()
     #plt.show()
 
@@ -48,7 +51,6 @@ filenames = ['sinx.png', 'sin1_x.png', 'sin8x.png']
 for func, filename in zip(functions, filenames):
     y_values = [func(alfa) for alfa in x]
     interpolate(x, y_values, 'linear', filename)
-
 
 def h1(x):
     if(x>=0 and x<1):
@@ -89,8 +91,8 @@ def h_functions(h, x, method, filename):
 
     script_dir = os.path.dirname(__file__)
     save_path = os.path.join(script_dir, filename)
-
     plt.savefig(save_path)
+    
     plt.clf()
     #plt.show()
 
@@ -100,3 +102,69 @@ h_functions(h1, x, 'linear', 'h1.png')
 h_functions(h2, x, 'linear', 'h2.png')
 h_functions(h3, x, 'linear', 'h3.png')
 h_functions(h4, x, 'linear', 'h4.png')
+
+#Przypadek w którym funkcja interpolowana nie będzie ciągła / punkty nie będą równoodległe od siebie
+N = 100
+x = np.linspace(-np.pi, np.pi, N)
+y = np.sin(x)
+
+remove_mask = (x >= -2) & (x <= 2)
+x = x[~remove_mask]
+y = y[~remove_mask]
+
+method = 'linear'
+interpolated_function = scp.interp1d(x, y, kind=method)
+
+x_int = np.linspace(x.min(), x.max(), 10*N)
+y_int = interpolated_function(x_int)
+y_true = np.sin(x_int)
+
+print(f"MSE: {metrics.mean_squared_error(y_pred=y_int, y_true=y_true):.8f}")
+plt.plot(x, y, 'go', label='Original Points')
+plt.plot(x_int, y_int, 'r-', label='Interpolation')
+plt.plot(x_int, y_true, 'y--', label='True Function')
+plt.grid(True)
+plt.legend()
+
+folder = os.path.dirname(os.path.abspath(__file__))
+filename = 'without_some_points.png'
+filepath = os.path.join(folder, filename)
+plt.savefig(filepath)
+plt.clf()
+plt.show()
+
+#Sprawdzanie wpływu ilości punktów na wskaźnik MSE
+print('Sprawdzanie wpływu ilości punktów na wskaźnik MSE')
+
+N_values = [5, 10, 20, 30, 70]
+
+filenames = ['n5.png', 'n10.png', 'n20.png', 'n30.png', 'n70.png']
+
+def interpolate_and_mse(N, filenames):
+    x = np.linspace(-np.pi, np.pi, N)
+    y = np.sin(x)
+
+    interpolated_function = interp1d(x, y, kind='linear')
+
+    x_true = np.linspace(-np.pi, np.pi, 10*N)
+    y_true = np.sin(x_true)
+
+    y_interp = interpolated_function(x_true)
+
+    mse = metrics.mean_squared_error(y_true, y_interp)
+    
+    print(f"N={N}: MSE = {mse:.8f}")
+
+    plt.plot(x, y, 'go', label='Original Points')
+    plt.plot(x_true, y_interp, 'r-', label='Interpolation')
+    plt.grid(True)
+    plt.legend()
+    plt.text(0.5, 1.05, f'MSE = {mse:.8f}', horizontalalignment='center', verticalalignment='center', transform=plt.gca().transAxes)
+    
+    script_dir = os.path.dirname(__file__)
+    save_path = os.path.join(script_dir, filenames[N_values.index(N)])
+    plt.savefig(save_path)
+    plt.clf()
+
+for N in N_values:
+    interpolate_and_mse(N,filenames)
